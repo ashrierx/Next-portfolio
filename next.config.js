bc5a09f51d7109a853c485d6a11fa66cc2042269
Ashley Judah <ajudah@Ashinator.local>
2025-12-11 15:08:50 -0500

    changing images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
-      domains: ['portfolioimagesnetlify.s3.us-west-2.amazonaws.com']
-    },
-  }
+  images: {
+    unoptimized: true,
+  },
+}
